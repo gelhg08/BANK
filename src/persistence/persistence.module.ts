9aa4b4581c967e8bba8448ecd74f701b33810a7d
riwip4-005 <angelicamhg31@gmail.com>
2024-04-25 06:28:13 -0500

    actualizar

--- src/persistence/persistence.module.ts
+++ src/persistence/persistence.module.ts
@@ -1,10 +1,3 @@
-/* 
-  1. The PersistenceModule is responsible for configuring the connection to the database. 
-  2. The MongooseModule.forRootAsync method is used to configure the connection to the database. 
-  3. The useFactory function is responsible for returning the connection configuration object. 
-  4. The inject property is used to inject the configuration object into the useFactory function.
- */
-
 import { Global, Module } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -29,17 +22,3 @@ import dbConfig from './db-config';
   ],
 })
 export class PersistenceModule {}
-
-/* 
-It defines a PersistenceModule with a single import: MongooseModule.forRootAsync(). This function configures the Mongoose module with a connection to a MongoDB database.
-
-Inside forRootAsync(), it defines a factory function that returns the configuration for the Mongoose module. This function takes a configService parameter, which is an instance of ConfigType<typeof dbConfig>. This means it's an object that has the same shape as the default export of ./db-config.
-
-The factory function destructures db and env from configService. These are presumably configuration values for the database and the environment.
-
-It defines a uriDb variable that holds the MongoDB connection string. If env is 'local', it constructs a connection string for a local MongoDB database. If env is not 'local', it constructs a connection string for a MongoDB Atlas database.
-
-It returns an object with a single property uri, which is the MongoDB connection string. This object is the configuration for the Mongoose module.
-
-
- */
